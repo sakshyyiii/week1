7615839282a448950f4a12a2acb3daa003525947
sakshyyiiii <kannojiyasakshi0106@gmail.com>
2026-07-26 13:23:56 +0530

    Excludes .env file and uploads day 1

---
+++ day1/hello_llm.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai?")
+
+client = Groq(api_key=my_api_key)
+
+model = "llama-3.3-70b-versatile"
+
+messages = [
+    {
+        "role": "user",
+        "content": "Do you know Sakshi?"
+    }
+]
+
+response = client.chat.completions.create(
+    model=model,
+    messages=messages
+)
+
+print(response)
+print("##########################################")
+print(response.choices[0].message.content)
